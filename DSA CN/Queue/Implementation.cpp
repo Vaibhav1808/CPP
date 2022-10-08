47699ede00c14ef3245de364e387e268fe997395
Vaibhav1808 <vaibhav.bt21ele@pec.edu.in>
2022-10-09 01:33:57 +0530

    Queue

--- DSA CN/Queue/Implementation.cpp
+++ DSA CN/Queue/Implementation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queue{
     int * arr;
-    int front;
+    int qfront;
     int rear;
     int size;
 
@@ -11,7 +11,48 @@ class Queue{
         Queue(){
             size = 100001;
             arr = new int[size];
-            front = 0;
+            qfront = 0;
             rear = 0;
         }
+    void enqueue(int data){
+        if(rear == size){
+            cout << "Queue is Full";
+        }
+        else{
+            arr[rear] = data; 
+            rear++;
+        }
+    }
+    int dequeue(){
+        if(qfront == rear){
+            return -1;
+        }
+        else{
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront == rear){
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }
+    int front(){
+        if(qfront == rear){
+            return -1;
+        }
+        else{
+            return arr[qfront];
+        }
+    }
+    bool isEmpty(){
+        if(qfront == rear){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 };
+
